056dd1991212c92275be141b709d7d10f1183dae
Atsushi <atu729@icloud.com>
2023-06-13 23:50:25 +0900

    add base file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import "./App.scss";
+import Chat from "./components/chat/Chat";
+import Sidebar from "./components/sidebar/Sidebar";
+
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* side bar */}
+      <Sidebar />
+
+      {/* home */}
+      <Chat />
+    </div>
+  );
 }
 
 export default App;

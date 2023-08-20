1f50fb9e2b43de986f9c9d85462619b6ab234309
Atsushi <atu729@icloud.com>
2023-08-20 21:36:13 +0900

    store and show input data

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import "./ChatMessage.scss";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import { Timestamp } from "firebase/firestore";
+import { Avatar } from "@mui/material";
 
-const ChatMessage = () => {
+type Props = {
+  timestamp: Timestamp;
+  message: string;
+  user: {
+    uid: string;
+    photo: string;
+    email: string;
+    displayName: string;
+  };
+};
+
+const ChatMessage = (props: Props) => {
+  const { message, timestamp, user } = props;
   return (
     <div className="message">
-      <AccountBoxIcon />
+      <Avatar src={user?.photo} />
       <div className="messageInfo">
         <h4>
-          <span className="messageTimestamp">2023/06/12</span>
+          {user?.displayName}
+          <span className="messageTimestamp">
+            {new Date(timestamp?.toDate()).toLocaleString()}
+          </span>
         </h4>
-
-        <p>message content</p>
+        <p>{message}</p>
       </div>
     </div>
   );
